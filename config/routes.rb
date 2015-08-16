1beab479304a7e3a2306652e1331b453fd669199
Steve Ellis <email@steveell.is>
2015-08-16 16:23:02 -0400

    websocket routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#show'
+  match "/websocket", to: WebsocketRails::ConnectionManager.new
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
