f9db4e02125834e8754f4ca153cf8df20d05c466
Desmond Bowe <desmond@crevalle.io>
2015-08-23 16:06:55 -0400

    clean up, reenable beat store

--- app/channels/beats_channel.rb
+++ app/channels/beats_channel.rb
@@ -1,9 +1,5 @@
 class BeatsChannel < ApplicationCable::Channel
   def subscribed
-    # name = params[:id].gsub(' ', '-')
-    # puts "streaming from #{name}"
-    # puts params[:id]
-    # stream_from 'beats'
   end
 
   def follow data
--- app/controllers/beats_controller.rb
+++ app/controllers/beats_controller.rb
@@ -7,17 +7,10 @@ class BeatsController < ApplicationController
   def create
     name = params[:id].gsub(' ', '-')
 
-    b = ActionCable.server.broadcast name, message: ''
-
-    # @beat = Beat.create name: name, ip: request.remote_ip, subscriber_count: channel.subscribers.count
+    subscriber_count = ActionCable.server.broadcast name, message: ''
+    @beat = Beat.create name: name, ip: request.remote_ip, subscriber_count: subscriber_count
 
     head :ok
-  end
-
-
-  def define_channel name
-    klass_name = "#{name.capitalize}Channel"
-    ActionCable.server.add_channel_class klass_name
   end
 end
 
